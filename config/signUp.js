f6304236d2f8bc409f0512014be9c685b7cee350
DlaminiTshaya <DlaminiNande99@gmail.com>
2021-10-07 15:05:54 +0200

    Sign Up

---
+++ config/signUp.js
@@ -0,0 +1,26 @@
+  
+import {firebase} from '../config/firebase'
+
+const signUp =((email,password, Confirmpassword)=>{
+    if(Confirmpassword !== password){
+        alert("password doen't match")
+    }else {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+           
+            firebase.firestore().collection('User').doc(email).set({
+               
+                email:email,
+                password:password})
+            alert("Account succesfully created ")
+          var user = userCredential.user;
+        
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      
+    };
+})
+export default signUp;
+
