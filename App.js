f3b271c9fb70c62e7088054c9ec86670478adb62
oloratopule <oloratopule716@gmail.com>
2021-10-07 11:40:23 +0200

    fixing-file-structure

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Register from './screens/Register'
+
 
 export default function App() {
   return (
