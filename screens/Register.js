d0194d0912a7c257908b953cdd7a512227517e87
DlaminiTshaya <DlaminiNande99@gmail.com>
2021-10-08 11:08:39 +0200

    error fixed

--- screens/Register.js
+++ screens/Register.js
@@ -5,6 +5,9 @@ import signUp from '../auth/signUp'
 
 
 export default function Register({navigation}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [Confirmpassword, setConfirmpassword] = useState('')
     const RegisterUser = () => {
         signUp(email,password, Confirmpassword);
     }
