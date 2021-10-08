71d4e10de75308b49825fbaf9887b2849d0c76c9
ThatoMahloko <thato732mahloko@gmail.com>
2021-10-08 10:47:11 +0200

    login, reset, password and reset email

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import React, { useSate } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, ImageBackground, Button, TouchableOpacity } from 'react-native';
 import login from '../auth/login';
 
