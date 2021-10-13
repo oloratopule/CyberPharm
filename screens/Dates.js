59262a3f8519bf27409ed317096d0b58c331eebb
oloratopule <oloratopule716@gmail.com>
2021-10-13 14:09:50 +0200

    Dates

--- screens/Dates.js
+++ screens/Dates.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -10,16 +10,17 @@ import {
   TextInput,
   Picker
 } from 'react-native';
-
+import { RadioButton } from 'react-native-paper';
 
 export default function Dates({ navigation }) {
   const [selectedValue, setSelectedValue] = useState('null');
   const [selectedMonth, setSelectedMonth] = useState('null');
-   const [getAge, setAge] = useState('');
+  const [getAge, setAge] = useState('');
   return (
     <SafeAreaView style={styles.container}>
-         <Picker
-     selectedMonth={selectedValue}
+
+      <Picker
+        selectedMonth={selectedValue}
         style={{ height: 30, width: 250, borderRadius: 20, }}
         onValueChange={(itemValue, itemIndex) => setSelectedMonth(itemValue)}
       >
@@ -35,33 +36,116 @@ export default function Dates({ navigation }) {
         <Picker.Item label="October, 2021" value="October" />
         <Picker.Item label="November, 2021" value="November" />
         <Picker.Item label="December, 2021" value="December" />
-       
       </Picker>
+
       <ScrollView
         horizontal={true}
         pagingEnabled={true}
         showsHorizontalScrollIndicator={false}
         style={styles.scrollContainer}>
         <View style={styles.cards}>
-          <View style={styles.card1}>
-            <Text style={styles.date}>13</Text>
+          <View style={styles.card1 }>
+            <Text style={styles.date}>1</Text>
+            <Text style={styles.day}>FRI</Text>
+          </View>
+          <View style={styles.card3}>
+
+            <Text style={styles.date}>2</Text>
+            <Text style={styles.day}>SAT</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>3</Text>
+            <Text style={styles.day}>SUN</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>4</Text>
             <Text style={styles.day}>MON</Text>
           </View>
-          <View style={styles.card2}>
-            <Text style={styles.date}>14</Text>
+          <View style={styles.card3}>
+            <Text style={styles.date}>5</Text>
             <Text style={styles.day}>TUE</Text>
           </View>
           <View style={styles.card3}>
-            <Text style={styles.date}>15</Text>
+
+            <Text style={styles.date}>6</Text>
             <Text style={styles.day}>WED</Text>
           </View>
           <View style={styles.card3}>
-            <Text style={styles.date}>16</Text>
+            <Text style={styles.date}>7</Text>
             <Text style={styles.day}>THUR</Text>
           </View>
           <View style={styles.card3}>
-            <Text style={styles.date}>17</Text>
+            <Text style={styles.date}>8</Text>
             <Text style={styles.day}>FRI</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>9</Text>
+            <Text style={styles.day}>SAT</Text>
+          </View>
+          <View style={styles.card2}>
+            <Text style={styles.date}>10</Text>
+            <Text style={styles.day}>SUN</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>11</Text>
+            <Text style={styles.day}>MON</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>12</Text>
+            <Text style={styles.day}>TUE</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>13</Text>
+            <Text style={styles.day}>WED</Text>
+            <View style={styles.card3}> 
+              <Text style={styles.date}>14</Text>
+              <Text style={styles.day}>THUR</Text>
+            </View>
+            {/* <View style={styles.card3}>
+            <Text style={styles.date}>15</Text>
+            <Text style={styles.day}>FRI</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>16</Text>
+            <Text style={styles.day}>SAT</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>18</Text>
+            <Text style={styles.day}>SUN</Text>
+          </View>
+          <View style={styles.card3}>
+
+            <Text style={styles.date}>19</Text>
+            <Text style={styles.day}>MON</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>20</Text>
+            <Text style={styles.day}>TUE</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>21</Text>
+            <Text style={styles.day}>WED</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>22</Text>
+            <Text style={styles.day}>THUR</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>23</Text>
+            <Text style={styles.day}>FRI</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>24</Text>
+            <Text style={styles.day}>SAT</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>25</Text>
+            <Text style={styles.day}>SUN</Text>
+          </View>
+          <View style={styles.card3}>
+            <Text style={styles.date}>26</Text>
+            <Text style={styles.day}>MON</Text>
+            </View> */}
           </View>
         </View>
       </ScrollView>
@@ -112,12 +196,12 @@ export default function Dates({ navigation }) {
       </View>
 
       <Text style={styles.time}> Patient Details</Text>
-     <Text style={styles.age}><b>Full Name</b></Text>
+      <Text style={styles.age}><b>Full Name</b></Text>
       <TextInput style={styles.input} placeholder="Full name" />
 
       <Text style={styles.age}><b>Age</b></Text>
       <Picker
-     selectedValue={selectedValue}
+        selectedValue={selectedValue}
         style={{ height: 30, width: 250, borderRadius: 20, }}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
       >
@@ -129,23 +213,23 @@ export default function Dates({ navigation }) {
 
       <Text style={styles.age}><b>Gender</b></Text>
       <RadioButton.Group
-          onValueChange={(newValue) => setAge(newValue)}
-          value={getAge}>
-          <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <RadioButton value="High" />
-            <Text style={{ paddingTop: '3%' }}>Male</Text>
-          </View>
+        onValueChange={(newValue) => setAge(newValue)}
+        value={getAge}>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <RadioButton value="High" />
+          <Text style={{ paddingTop: '3%' }}>Male</Text>
+        </View>
 
-          <View style={{ display: 'flex', flexDirection: 'row' }}>
-            <RadioButton value="Medium" />
-            <Text style={{ paddingTop: '3%' }}>Female</Text>
-          </View>
-        </RadioButton.Group>
-        <Text style={styles.age}><b>Write your problem</b></Text>
-              <TextInput style={styles.problem} placeholder="Write your problem" />
-                  <TouchableOpacity style={styles.btn}>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <RadioButton value="Medium" />
+          <Text style={{ paddingTop: '3%' }}>Female</Text>
+        </View>
+      </RadioButton.Group>
+      <Text style={styles.age}><b>Write your problem</b></Text>
+      <TextInput style={styles.problem} placeholder="Write your problem" />
+      <TouchableOpacity style={styles.btn}>
         <View style={styles.app}> Set Appointment</View>
-        </TouchableOpacity>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -158,7 +242,6 @@ const styles = StyleSheet.create({
     bottom: 10,
   },
   scrollContainer: {
-    width: 400,
     height: 120,
   },
   cards: {
@@ -234,28 +317,28 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
     border: '1px solid gray',
   },
-  age:{
-    marginTop:10,
-    marginLeft:10
+  age: {
+    marginTop: 10,
+    marginLeft: 10
   },
-  problem:{
-      width: 250,
+  problem: {
+    width: 250,
     height: 90,
     marginRight: 35,
     paddingLeft: 15,
     border: '1px solid gray',
-    marginTop:10
+    marginTop: 10
   },
   btn: {
-      backgroundColor: 'background: rgba(5, 78, 222, 0.7);',
+    backgroundColor: 'background: rgba(5, 78, 222, 0.7);',
     width: 280,
     height: 40,
-    borderRadius:20,
-    marginTop:10
+    borderRadius: 20,
+    marginTop: 10
   },
-    app:{
-    marginLeft:90,
-    marginTop:5,
-    color:'white'
+  app: {
+    marginLeft: 90,
+    marginTop: 5,
+    color: 'white'
   }
 });
