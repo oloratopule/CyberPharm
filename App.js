2d2647f481639f2032a4c992c7be68ea14194c9f
oloratopule <oloratopule716@gmail.com>
2021-10-13 10:00:15 +0200

    yes

--- App.js
+++ App.js
@@ -7,40 +7,35 @@ import SymptopmChecker from './screens/SymptopmChecker';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Login from './screens/Login';
-import Register from './screens/Register';
-import Home from './screens/Home';
-import ResetPassword from './screens/ResetPassword';
-import SOS from './screens/SOS';
-import Doctors from './screens/Doctors';
-import Appointment from './screens/Appointment';
-import Dates from './screens/Dates';
-import Contact from './screens/Contacts';
-import ContactForm from './screens/ContactForm';
+// import Login from './screens/Login';
+// import Register from './screens/Register';
+// import Home from './screens/Home';
+// import ResetPassword from './screens/ResetPassword';
+// import SOS from './screens/SOS';
+// import Doctors from './screens/Doctors';
+// import Appointment from './screens/Appointment';
+// import Dates from './screens/Dates';
+// import Contact from './screens/Contacts';
+// import ContactForm from './screens/ContactForm';
 
 const Stack = createNativeStackNavigator();
 export default function App({ navigation }) {
 
   return (
-      <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator>
-      {/* <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} /> 
-        <Stack.Screen name="Home" component={Home} /> 
-        <Stack.Screen name="ResetPassword" component={ResetPassword} />  */}
-         <Stack.Screen name="Contact" component={Contact} />
-         {/* <Stack.Screen name="SOS" component={SOS} />
-        <Stack.Screen name="Doctors" component={Doctors} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ResetPassword" component={ResetPassword} />
+        <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen name="SOS" component={SOS} />
+        {/* <Stack.Screen name="Doctors" component={Doctors} />
         <Stack.Screen name="Appointment" component={Appointment} />
         <Stack.Screen name="Dates" component={Dates} /> */}
       </Stack.Navigator>
     </NavigationContainer>
 
-
-    // </NavigationContainer>
-    // <View>
-    //   <SymptopmChecker />
-    // </View>
   );
 }
 
